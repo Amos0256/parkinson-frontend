1af3ce7c1f6ab7c0af911443701fb3a5756be615
409410038 <97959881+409410038@users.noreply.github.com>
2022-11-13 20:41:47 +0800

    [add] doctor page

--- src/pages/Doctor/List/index.jsx
+++ src/pages/Doctor/List/index.jsx
@@ -1,3 +1,13 @@
-export default function Doctor(){
-    return <div>List</div>
+import Header from "../../components/Header";
+import Table from "../../components/Table";
+import DialogDemo from "../../components/Dialog";
+
+export default function Doctor() {
+  return (
+    <div>
+      <Header title={"主頁"}/>
+      <DialogDemo title={"dialogtest"}/>
+      <Table title={"tabletest"}/>
+    </div>
+  );
 }
