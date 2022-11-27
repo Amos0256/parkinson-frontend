3d6dd9e1e52ac61a7ee8ee2e60251a2258b37534
409410038 <97959881+409410038@users.noreply.github.com>
2022-11-27 23:17:39 +0800

    [add] header logout, show profile
    個人資料只有先用抓到的資料，還沒有做美化

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,39 @@
 import useAuth from "hooks/useAuth";
+import { useState } from 'react';
+import { useEffect } from "react";
 import { Button } from "primereact/button";
 import { Menubar } from "primereact/menubar";
 import { useNavigate, useLocation} from 'react-router-dom';
 import { SplitButton } from "primereact/splitbutton";
 import { Toolbar } from "primereact/toolbar";
+import { Dialog } from 'primereact/dialog';
 import React from "react";
 import "./header.css";
 import icon from "./parkinson.png";
+import api from "utils/api";
 
 export default function Header({ title }) {
   const navigate = useNavigate("");
-  const { user } = useAuth();
+  const { user, isLogin, loading, logout } = useAuth();
+  const [profileShow, setProfileShow] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    if (loading) return;
+    if (isLogin){
+      api("user-info", "GET")
+      .then((json) => {
+        setCurrentUser(json);
+      })
+      .catch((e) => {
+        alert(e.message);
+        navigate("/login");
+      });
+    }
+    else{
+      navigate("/login");
+    }
+  }, [isLogin, loading]);
 
   const info = [
     {
@@ -18,13 +41,14 @@ export default function Header({ title }) {
       icon: "pi pi-fw pi-file-edit",
       command: () => {
         //direct to personal information
+        setProfileShow(true);
       }
     },
     {
       label: "登出",
       icon: "pi pi-fw pi-power-off",
       command: () => {
-          //logout
+        logout();
       }
     },
   ];
@@ -80,6 +104,13 @@ export default function Header({ title }) {
     <div>
       <div className="card">
         <Toolbar left={start}  right={end} style={{ padding: "0.5rem" }} />
+        <Dialog 
+          header="個人資料"
+          visible={profileShow}
+          style={{ width: '60rem', height: 'auto'}}
+          onHide={() => setProfileShow(false)}>
+            {JSON.stringify(currentUser)}
+          </Dialog>
       </div>
     </div>
   );
