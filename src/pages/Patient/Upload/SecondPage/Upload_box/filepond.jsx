cb62695e930315979b656f150ea502ff9d8f234c
xwk1246 <xwk1246@csie.io>
2022-11-28 08:59:47 +0800

    [fix] filepond upload failed

--- src/pages/Patient/Upload/SecondPage/Upload_box/filepond.jsx
+++ src/pages/Patient/Upload/SecondPage/Upload_box/filepond.jsx
@@ -1,51 +1,46 @@
-import React, { useRef, useState, useContext } from 'react';
-import { browserHistory, Router, Route } from 'react-router';
-import { FilePond, registerPlugin } from 'react-filepond';
-import 'filepond/dist/filepond.min.css';
+import React, { useRef, useState, useContext } from "react";
+import { browserHistory, Router, Route } from "react-router";
+import { FilePond, registerPlugin } from "react-filepond";
+import "filepond/dist/filepond.min.css";
 
-import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 // import { usePage } from "@inertiajs/inertia-react";
 
-import './UploadBox.css';
+import "./UploadBox.css";
 // import { create } from 'filepond';
 // import { AuthContext } from "hooks/useAuth";
 
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
-
-
 export default function FileUpload() {
-    const [files, setFiles] = useState([]);
-    // const auth_context = useContext(AuthContext);
-    // const { csrf_token } = usePage().props;
-    const token = localStorage.getItem("token");
-    return (
-        <div className='filepond-upload'>
-            <FilePond 
-                files={files}
-                // onupdateFiles={setFiles}
-                server={{
-                    url: "http://140.123.242.78/api/upload-video",
-                    headers: {
-                        accept: "application/json", 
-                        "content-type": "application/json;charset=UTF-8",
-                        "Authorization": "Bearer " + token,
-                    }
-                }}
-                name="video"
-                // acceptedFileTypes={['video/mp4']}
-                // allowDrop={true}
-                // dropValidation={true}
-                // allowMultiple={true}
-                // maxFiles={3}
-                instantUpload={false}
-                labelIdle='請拖移檔案至此 或是 <span class="filepond--label-action">瀏覽</span>'
-            />
-            
-        </div>
-    )
+  const [files, setFiles] = useState([]);
+  // const auth_context = useContext(AuthContext);
+  // const { csrf_token } = usePage().props;
+  const token = localStorage.getItem("token");
+  return (
+    <div className="filepond-upload">
+      <FilePond
+        files={files}
+        // onupdateFiles={setFiles}
+        server={{
+          url: "http://140.123.242.78/api/upload-video",
+          headers: {
+            accept: "application/json",
+            Authorization: "Bearer " + token,
+          },
+        }}
+        name="video"
+        acceptedFileTypes={["video/mp4"]}
+        allowDrop={true}
+        dropValidation={true}
+        maxFiles={3}
+        instantUpload={false}
+        labelIdle='請拖移檔案至此 或是 <span class="filepond--label-action">瀏覽</span>'
+      />
+    </div>
+  );
 }
 
 // need process chunks if file is very big?
