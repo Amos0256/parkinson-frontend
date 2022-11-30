3092301ac8ff590e5b4bdb4d1bfb67199750bc66
409410038 <97959881+409410038@users.noreply.github.com>
2022-11-30 11:48:10 +0800

    [add] doctor page patient list fetch data
    can fetch data using api and link the button to the corresponding /each page

--- src/pages/Doctor/List/Table/index.jsx
+++ src/pages/Doctor/List/Table/index.jsx
@@ -6,20 +6,43 @@ import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { ProgressBar } from 'primereact/progressbar';
 import moment from "moment";
+import { useNavigate } from "react-router-dom";
+import useAuth from "hooks/useAuth";
+import api from "utils/api";
 
 export default function Table({ title }) {
-    const [patients, setPatients] = useState(null);
     const [selectedPatients, setSelectedPatients] = useState(null);
+    const [patients, setPatients] = useState({});
+    const { user, isLogin, loading} = useAuth();
+    const navigate = useNavigate();
     const [filters, setFilters] = useState({
         'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
         'name': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-        'date': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] },
-        'lastUpload': {operator: FilterOperator.AND, constraints: [{ value:null, matchMode:FilterMatchMode.DATE_IS }] }, 
-        'activity': { value: null, matchMode: FilterMatchMode.BETWEEN }
     });
     
+    function checkList(patient_id) {
+        let pathname = '/doctor/each/' + patient_id.toString();
+        navigate(pathname);
+    }
+
+    useEffect(() => {
+        if (loading) return;
+        if (isLogin){
+          api("assoc-record", "GET")
+          .then((json) => {
+            setPatients(json.patients);
+          })
+          .catch((e) => {
+            alert(e.message);
+            navigate("/doctor");
+          });
+        }
+        else{
+          navigate("/login");
+        }
+      }, [isLogin, loading]);
+
     const [globalFilterValue, setGlobalFilterValue] = useState('');
-    const [loading, setLoading] = useState(false);
     const datalist =
     [
         {
@@ -85,19 +108,20 @@ export default function Table({ title }) {
             </div>
         )
     }
+    const nameBodyTemplate = (rowData) => {
+        return (rowData.name);
+    }
     const dateBodyTemplate = (rowData) => {
-        return formatDate(rowData.date);
+        return formatDate(rowData.updated_at);
     }
     const lastUploadBodyTemplate = (rowData) => {
-        return formatTime(rowData.lastUpload);
+        return formatTime(rowData["missions"][0]["updated_at"]);
     }
-
-    const activityBodyTemplate = (rowData) => {
-        return <ProgressBar value={rowData.activity} showValue={1} color='#4FC0FF' displayValueTemplate={displayValueTemplate} ></ProgressBar>;
-    }
-
-    const actionBodyTemplate = () => {
-        return <Button label="檢視結果" style={{ background: '#f8f9fa', color: 'rgb(82, 79, 79)', border: '0px' }} className='button-check'></Button>;
+    const progressBodyTemplate = (rowData) => {
+        return <ProgressBar value={rowData["missions"][0]["id"]} color='#4FC0FF' displayValueTemplate={displayValueTemplate} ></ProgressBar>;
+    }  //just use id to represent the progress temporarily
+    const actionBodyTemplate = (rowData) => {
+        return <Button label="檢視" onClick={() => {checkList(rowData.id);}} style={{ background: '#f8f9fa', color: 'rgb(82, 79, 79)', border: '0px' }} className='button-check'></Button>;
     }
 
     const header = renderHeader();
@@ -105,15 +129,14 @@ export default function Table({ title }) {
     return(
         <div className="datatable-doc-demo">
             <div className="card">
-                <DataTable value={datalist} className="p-datatable-customers" header={header}
+                <DataTable value={patients} className="p-datatable-customers" header={header}
                     dataKey="id" rowHover selection={selectedPatients} onSelectionChange={e => setSelectedPatients(e.value)}
-                    filters={filters} filterDisplay="menu" loading={loading} responsiveLayout="scroll"
-                    globalFilterFields={['name']} emptyMessage="沒有任何患者資料"
-                    currentPageReportTemplate="顯示 {first} 到 {last} (共 {totalRecords} 項) ">
-                    <Column field="name" header="姓名" sortable style={{ minWidth: '14rem' }} />
-                    <Column field="date" header="上次就診日期" sortable filterField="date" dataType="date" style={{ minWidth: '8rem' }} body={dateBodyTemplate}/>
-                    <Column field="date" header="最新影片上傳時間" sortable filterField="date" dataType="date" style={{ minWidth: '14rem' }} body={lastUploadBodyTemplate} />
-                    <Column field="activity" header="完成進度" sortable style={{ minWidth: '10rem' }} body={activityBodyTemplate} />
+                    filters={filters} responsiveLayout="scroll"
+                    globalFilterFields={['name']} emptyMessage="沒有任何患者資料">
+                    <Column field="name" header="姓名" sortable style={{ minWidth: '14rem' }} body={nameBodyTemplate}/>
+                    <Column field="updated_at" header="上次就診日期" sortable dataType="date" style={{ minWidth: '8rem' }} body={dateBodyTemplate}/>
+                    <Column field="lastUpload" header="最新影片上傳時間" sortable dataType="date" style={{ minWidth: '14rem' }} body={lastUploadBodyTemplate} />
+                    <Column field="activity" header="完成進度" sortable style={{ minWidth: '10rem' }} body={progressBodyTemplate} />
                     <Column headerStyle={{ width: '10rem', textAlign: 'center' }} bodyStyle={{ textAlign: 'center', overflow: 'visible' }} body={actionBodyTemplate} />
                 </DataTable>
             </div>
