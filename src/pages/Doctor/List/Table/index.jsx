f327e00e0ac6268ad39519bb1540df8776ff6741
409410038 <97959881+409410038@users.noreply.github.com>
2022-12-10 22:49:34 +0800

    [mod] column sort function
    use mission's uploaded_records to sort the column of progress

--- src/pages/Doctor/List/Table/index.jsx
+++ src/pages/Doctor/List/Table/index.jsx
@@ -38,7 +38,6 @@ export default function Table({ patients }) {
         return moment(value).format("YYYY/MM/D, HH:mm");
     }
     const displayValueTemplate = (value) => {
-
         return (
             <React.Fragment>
                 {value}%
@@ -92,7 +91,32 @@ export default function Table({ patients }) {
     }  
     const actionBodyTemplate = (rowData) => {
         return <Button label="檢視" onClick={() => {checkList(rowData.id);}} style={{ background: '#f8f9fa', color: 'rgb(82, 79, 79)', border: '0px' }} className='button-check'></Button>;
-    }       
+    }
+    function customSort(event) {
+        let data = [...patients];
+        data.sort((data1, data2) => {
+            let l1 = data1.missions.length-1;
+            let l2 = data2.missions.length-1;
+            const value1 = data1.missions[l1][event.field];
+            const value2 = data2.missions[l2][event.field];
+            let result = null;
+
+            if (value1 == null && value2 != null)
+                result = -1;
+            else if (value1 != null && value2 == null)
+                result = 1;
+            else if (value1 == null && value2 == null)
+                result = 0;
+            else if (typeof value1 === 'string' && typeof value2 === 'string')
+                result = value1.localeCompare(value2, undefined, { numeric: true });
+            else
+                result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+
+            return (event.order * result);
+        });
+
+        return data;
+    }   
 
     const header = renderHeader();
 
@@ -104,9 +128,9 @@ export default function Table({ patients }) {
                     filters={filters} responsiveLayout="scroll"
                     globalFilterFields={['name']} emptyMessage="沒有任何患者資料">
                     <Column field="name" header="姓名" sortable style={{ minWidth: '14rem' }} body={nameBodyTemplate}/>
-                    <Column field="created_at" header="上次就診日期" sortable dataType="date" style={{ minWidth: '8rem' }} body={dateBodyTemplate}/>
-                    <Column field="updated_at" header="最新影片上傳時間" sortable dataType="date" style={{ minWidth: '14rem' }} body={lastUploadBodyTemplate} />
-                    <Column field="activity" header="完成進度" sortable style={{ minWidth: '10rem' }} body={progressBodyTemplate} />
+                    <Column field="created_at" header="上次就診日期" sortable sortFunction={customSort} dataType="date" style={{ minWidth: '8rem' }} body={dateBodyTemplate}/>
+                    <Column field="updated_at" header="最新影片上傳時間" sortable sortFunction={customSort} dataType="date" style={{ minWidth: '14rem' }} body={lastUploadBodyTemplate} />
+                    <Column field="uploaded_records" header="完成進度" sortable sortFunction={customSort} style={{ minWidth: '10rem' }} body={progressBodyTemplate} />
                     <Column headerStyle={{ width: '10rem', textAlign: 'center' }} bodyStyle={{ textAlign: 'center', overflow: 'visible' }} body={actionBodyTemplate} />
                 </DataTable>
             </div>
